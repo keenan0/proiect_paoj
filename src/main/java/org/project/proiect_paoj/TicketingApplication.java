25149bde2649195d6e544b4c842f0cd270e91130
keenan0 <tumbrlzet0680@gmail.com>
2025-03-28 19:25:44 +0200

    spring-boot intial setup

--- src/main/java/org/project/proiect_paoj/TicketingApplication.java
+++ src/main/java/org/project/proiect_paoj/TicketingApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProiectPaojApplication {
-
+public class TicketingApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ProiectPaojApplication.class, args);
+        SpringApplication.run(TicketingApplication.class, args);
     }
-
 }
--- src/test/java/org/project/proiect_paoj/TicketingApplicationTests.java
+++ src/test/java/org/project/proiect_paoj/TicketingApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProiectPaojApplicationTests {
+class TicketingApplicationTests {
 
     @Test
     void contextLoads() {
