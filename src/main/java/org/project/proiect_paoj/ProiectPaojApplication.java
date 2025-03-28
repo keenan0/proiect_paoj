7d04109c7315354c27afb8ced0ce9ffe4747f21b
keenan0 <tumbrlzet0680@gmail.com>
2025-03-28 19:16:37 +0200

    spring-boot intial setup

---
+++ src/main/java/org/project/proiect_paoj/ProiectPaojApplication.java
@@ -0,0 +1,13 @@
+package org.project.proiect_paoj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProiectPaojApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProiectPaojApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/project/proiect_paoj/ProiectPaojApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.proiect_paoj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProiectPaojApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
